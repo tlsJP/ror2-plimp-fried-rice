f6ddda3f16fab737b78d75904f17b071c31ae937
John Albert <jpalbert81@gmail.com>
2022-11-11 17:53:33 -0800

    risk of options added

--- PlimpFriedRice.cs
+++ PlimpFriedRice.cs
@@ -1,20 +1,21 @@
 
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
-using BepInEx.Bootstrap;
 using R2API;
 using R2API.Networking;
 using R2API.Utils;
 using RiskOfOptions;
+using RiskOfOptions.Options;
 using System.IO;
 using System.Reflection;
+using UnityEngine;
 
 namespace com.thejpaproject
 {
   [BepInDependency(R2API.R2API.PluginGUID)]
   [BepInDependency("com.rune580.riskofoptions")]
   [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-
   [R2APISubmoduleDependency(nameof(SoundAPI), nameof(ItemAPI), nameof(NetworkingAPI))]
 
   public class PlimpFriedRice : BaseUnityPlugin
@@ -25,19 +26,17 @@ namespace com.thejpaproject
     public const string PluginName = "PlimpFriedRice";
     public const string PluginVersion = "1.0.2";
 
-    private static readonly bool s_rooEnabled = Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
+    // private static readonly bool s_rooEnabled = Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
+
+    private static ConfigEntry<bool> s_enabled;
 
     public void Awake()
     {
       _logger = BepInEx.Logging.Logger.CreateLogSource("PlimpFriedRice");
 
-      if (s_rooEnabled)
-      {
-        ConfigureRiskOfOptions();
-      }
 
+      ConfigureRiskOfOptions();
 
-      _logger.LogInfo("trying...");
 
 
       using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("com.thejpaproject.plimpfriedrice.bnk"))
@@ -52,14 +51,15 @@ namespace com.thejpaproject
 
       On.RoR2.Orbs.OrbEffect.Start += OrbStart;
 
-      _logger.LogInfo("Completed");
+      _logger.LogInfo("Plimp has been deep fried");
     }
 
     private static void OrbStart(On.RoR2.Orbs.OrbEffect.orig_Start orig, RoR2.Orbs.OrbEffect self)
     {
       orig(self);
-      _logger.LogInfo($" {self.name} - OrbStart");
-      if (self.name.StartsWith("MissileVoidOrbEffect"))
+      var enabled = ((ConfigEntry<bool>)s_enabled).Value;
+
+      if (enabled && self.name.StartsWith("MissileVoidOrbEffect"))
       {
         RoR2.Util.PlaySound("deepfriedplimp", self.gameObject);
       }
@@ -68,8 +68,38 @@ namespace com.thejpaproject
 
     void ConfigureRiskOfOptions()
     {
+
+      LoadSprite();
       ModSettingsManager.SetModDescription("Enable/disable plimp fried rice");
+
+      s_enabled = this.Config.Bind("General", "Plimp Fried Rice", false, "Do you want plimp fried rice? Do you like your plimps deep fried?");
+      ModSettingsManager.AddOption(new CheckBoxOption(s_enabled));
+      HandleEvent(s_enabled, null);
+      s_enabled.SettingChanged += HandleEvent;
+    }
+
+    void HandleEvent(object x, System.EventArgs _)
+    {
+      var enabled = ((ConfigEntry<bool>)x).Value;
+    }
+
+    private void LoadSprite()
+    {
+      using var stream = GetType().Assembly.GetManifestResourceStream("plimp");
+      var texture = new Texture2D(0, 0);
+      var imgdata = new byte[stream.Length];
+      stream.Read(imgdata, 0, imgdata.Length);
+      if (ImageConversion.LoadImage(texture, imgdata))
+      {
+        var sprite = Sprite.Create(
+                   texture,
+                   new Rect(0, 0, texture.width, texture.height),
+                   new Vector2(0, 0)
+                 );
+        ModSettingsManager.SetModIcon(sprite);
+      }
     }
 
   }
+
 }
